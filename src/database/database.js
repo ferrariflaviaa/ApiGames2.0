5e9f643f93dabd4e91f995cc9dbad719e13d3e88
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-11-20 16:44:32 -0300

    feat: start project

---
+++ src/database/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize")
+
+const connection = new Sequelize("apigames", "root", '123456', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false,
+  port: 3312,
+  timezone: "-03:00"
+});
+
+module.exports = connection;
